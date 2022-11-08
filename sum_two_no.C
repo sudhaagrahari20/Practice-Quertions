3a54dd6bffb7b18a21d79b60741f267827768ab9
sudhaagrahari20 <117359974+sudhaagrahari20@users.noreply.github.com>
2022-11-08 21:03:38 +0530

    Create sum_two_no.C

---
+++ sum_two_no.C
@@ -0,0 +1,21 @@
+
+  
+  #include <stdio.h>
+
+int main()
+{
+  int a,b,sum,sub,mul;
+  printf("enter the number:");
+  scanf("%d%d", &a,&b);
+  sum=a+b;
+  sub=a-b;
+  mul=a*b;
+  
+  
+  printf("sum=%d",sum);
+  printf("\nsub=%d",sub);
+  printf("\nmul=%d",mul);
+  
+}
+
+
