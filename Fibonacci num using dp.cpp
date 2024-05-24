c2f72d12dbd6b859ae15339fc50d8e9235edcb37
Sudha Agrahari <117359974+sudhaagrahari20@users.noreply.github.com>
2024-05-24 15:10:21 +0530

    Create Fibonacci num using dp.cpp

---
+++ Fibonacci num using dp.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int topDownSolve(int n, vector<int>& dp){
+        if(n==1 || n==0) return n;
+        if(dp[n]!=-1) return dp[n];
+        dp[n]=topDownSolve(n-1,dp)+topDownSolve(n-2,dp);
+        return dp[n];
+    }    
+
+    int fib(int n) {
+        vector<int>dp(n+1, -1);
+        int ans = topDownSolve(n, dp);
+        return ans;
+        
+    }
+};
