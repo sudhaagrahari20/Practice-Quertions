caacd18758ecbd25f6afaaf4a3ed88ee7a2a14ab
Sudha Agrahari <117359974+sudhaagrahari20@users.noreply.github.com>
2024-05-24 16:46:09 +0530

    Create Coin Change using DP.cpp

---
+++ Coin Change using DP.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+int solveMem(vector<int>& coins, int amount, vector<int>& dp) {
+        //base case
+        if(amount == 0) {
+            return 0;
+        }
+        if(amount < 0 ) {
+            return INT_MAX;
+        }
+        //ans already exists
+        if(dp[amount] != -1) {
+            return dp[amount];
+        }
+
+        int mini = INT_MAX;
+        for(int i=0; i<coins.size(); i++) {
+            int ans = solveMem(coins, amount - coins[i], dp);
+            if(ans != INT_MAX) {
+                mini = min(mini,1+ ans);
+            }
+        }
+        dp[amount] = mini;
+        return dp[amount];
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount+1, -1);
+        int ans = solveMem(coins,amount, dp);
+        if(ans == INT_MAX)
+            return -1;
+        else 
+            return ans;       
+    }
+};
